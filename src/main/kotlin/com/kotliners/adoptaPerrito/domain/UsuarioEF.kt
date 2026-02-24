d3367a8d2d2153eed411d838d9d9a51409d2ead1
julietanilem <julietanilem@gmail.com>
2026-02-23 22:27:59 -0600

    Feat(domain/UsuarioEF): Creación de la clase

--- src/main/kotlin/com/kotliners/adoptaPerrito/domain/UsuarioEF.kt
+++ src/main/kotlin/com/kotliners/adoptaPerrito/domain/UsuarioEF.kt
@@ -1,4 +1,20 @@
 package com.kotliners.adoptaPerrito.domain
 
-class UsuarioEF {
+import com.kotliners.adoptaPerrito.dto.request.CreateUsuarioRequest
+import java.util.UUID
+
+/**
+ * Función de extensión que transforma un CreateUsuarioRequest
+ * en una instancia de Usuario, generando un identificador único.
+ */
+fun CreateUsuarioRequest.toUsuario(): Usuario {
+
+    // Creamos el objeto de dominio usando los datos del DTO y un id aleatorio
+    return Usuario(
+        id = "id-random-" + UUID.randomUUID().toString(),
+        nombre = this.nombre,
+        email = this.email,
+        cp = this.cp
+    )
 }
+
