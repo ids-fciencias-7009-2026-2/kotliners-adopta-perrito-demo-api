cceca8377932fc37212dd490a7e1d06b025a2288
jzarcoo <zarco_romero_jose_antonio@comunidad.unam.mx>
2026-03-16 23:24:18 -0600

    database: update usuario table

--- database/usuario.sql
+++ database/usuario.sql
@@ -2,12 +2,19 @@ DROP TABLE IF EXISTS Usuario;
 
 CREATE TABLE Usuario (
     IDUsuario SERIAL PRIMARY KEY,
+    curp VARCHAR(18) NOT NULL UNIQUE,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    rol VARCHAR(20) NOT NULL,
+    foto_perfil VARCHAR(255), -- url
     nombres VARCHAR(100) NOT NULL,
     apellido_paterno VARCHAR(100) NOT NULL,
     apellido_materno VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     codigo_postal VARCHAR(10) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
     token VARCHAR(255)
 );
+
+-- Restricción para el campo 'rol' para que solo acepte 'cuidador' o 'adoptante'
+ALTER TABLE Usuario
+    ADD CONSTRAINT rol_check CHECK (rol IN ('cuidador', 'adoptante'));
