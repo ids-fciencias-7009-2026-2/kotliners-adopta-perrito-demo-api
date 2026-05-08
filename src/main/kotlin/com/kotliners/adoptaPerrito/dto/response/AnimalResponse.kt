902fba09dd0c87eb788a772219a06e3370bcfb3b
Luis Angel Casique Abraham <masunni@ciencias.unam.mx>
2026-05-08 00:54:50 -0600

    nuevas clases y actualizacion: CreateAnimalRequest.kt y AnimalResponse.kt ademas de actualizacion de AnimalService.kt y AnimalController.kt

--- src/main/kotlin/com/kotliners/adoptaPerrito/dto/response/AnimalResponse.kt
+++ src/main/kotlin/com/kotliners/adoptaPerrito/dto/response/AnimalResponse.kt
@@ -1,4 +1,37 @@
 package com.kotliners.adoptaPerrito.dto.response
 
-class AnimalResponse {
-}
+import com.kotliners.adoptaPerrito.domain.Animal
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class AnimalResponse (
+
+    val id: String,
+    val nombre: String,
+    val especie: String,
+    val raza: String?,
+    val fechaNacimiento: LocalDate,
+    val sexo: String,
+    val descripcion: String,
+    val estatus: String,
+    val esterilizado: Boolean,
+    val usuarioId: String,
+    val fechaRegistro: LocalDateTime
+)
+
+/**
+ * Convierte un Animal de dominio a AnimalResponse.
+ */
+fun Animal.toAnimalResponse() = AnimalResponse(
+    id = this.id ?: "",
+    nombre = this.nombre,
+    especie = this.especie,
+    raza = this.raza,
+    fechaNacimiento = this.fechaNacimiento,
+    sexo = this.sexo.name,
+    descripcion = this.descripcion,
+    estatus = this.estatus.name,
+    esterilizado = this.esterilizado,
+    usuarioId = this.usuarioId,
+    fechaRegistro = this.fechaRegistro
+)
